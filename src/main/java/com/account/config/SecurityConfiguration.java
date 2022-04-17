de920485d2d7a6600f268e0beee2133d796af591
abdussalam <abdus.salam@teamapt.com>
2022-04-17 15:01:33 +0500

    added controller advice and application.yml file

---
+++ src/main/java/com/account/config/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.account.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class SecurityConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Bean
+    public Authentication authentication() {
+        return null;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.formLogin().
+                loginPage("/login").and().authorizeRequests()
+                .antMatchers(new String[]{
+                        "/api/v1/otp/*"
+                }).permitAll()
+                .anyRequest().authenticated().and()
+                .csrf().disable();
+    }
+
+}
